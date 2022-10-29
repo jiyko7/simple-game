00ae5c350b15087907b3ce3a0951b3194454aa6c
Woong <jiyko7@kyonggi.ac.kr>
2022-10-29 14:03:40 +0900

    unito
    반다이 남코 현역 디렉터가 알려주는 유니티 게임 제작 입문 unito 게임 클론코딩

---
+++ Assets/goal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal : MonoBehaviour
+{
+    private bool is_collided = false;
+
+    public float GOAL_MIN = 5.0f;
+    public float GOAL_MAX = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float rnd = Random.Range(GOAL_MIN, GOAL_MAX);
+        this.transform.position = new Vector3(rnd, -1.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        this.is_collided = true;
+    }
+
+    void OnGUI()
+    {
+        if (is_collided)
+        {
+            GUI.Label(new Rect(Screen.width / 2, 80, 100, 20), "성공");
+        }
+    }
+}
